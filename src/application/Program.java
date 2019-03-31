946f72efde4b1cf07c22d385f3b044cf9b9fc379
Luiz Fernando de Morais <luiz.ado@hotmail.com>
2019-03-31 13:14:31 -0300

    Project done

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,27 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Tablet", 560.00));
+		
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		for(Product p : list) {
+			System.out.println(p);
+		}
 
 	}
 
